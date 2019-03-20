6be1bea60a636506ea237f8c7909e71d4c000d55
lzl96666 <83413770@qq.com>
2019-03-20 08:40:10 +0800

    优化（图片 cdn）

--- public/js/mobile-common.js
+++ public/js/mobile-common.js
@@ -9,25 +9,31 @@ function fnOpenMenu() {
 }
 // 回到首页
 function fnToIndex() {
+    fnOpenMenu();
     window.location.href = "/"
 }
 // 产品页
 function fnToProduct() {
+    fnOpenMenu();
     window.location.href = "/mobileSubView/product/product.html"
 }
 // 实例报告
 function fnToSampleReport() {
+    fnOpenMenu();
     window.location.href = "/mobileSubView/sample-report/sample-report.html"
 }
 // 服务流程
 function fnToProcess() {
+    fnOpenMenu();
     window.location.href = "/mobileSubView/process/process.html"
 }
 // 关于我们
 function fnToAboutUs() {
+    fnOpenMenu();
     window.location.href = "/mobileSubView/about/about.html"
 }
 // 
 function fnToTop() {
+    fnOpenMenu();
     $('html , body').animate({ scrollTop: 0 }, 'slow');
 }
